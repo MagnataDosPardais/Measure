3ab4348b6259113de26462ecdef9baf53446531b
Marco A. Z. Bee <80833661+MagnataDosPardais@users.noreply.github.com>
2023-09-25 15:50:39 -0300

    Update MEASURE.ino

--- MEASURE.ino
+++ MEASURE.ino
@@ -16,8 +16,14 @@ const int NOTES[QNOTES] = {BASSNOTE, MIDNOTE, TREBLENOTE};
 VL53L0X_RangingMeasurementData_t measure;
 bool debugTof = false;
 unsigned int currentValue;
-unsigned long int average;
-float td;
+byte readNumb = 20;
+unsigned int averageReadBase;
+unsigned int averageReadBlock;
+unsigned int blockHeight;
+unsigned int calVvc = 100; //
+int c;
+//byte readNumbC = 20;
+//int timeoutTof = 50;
 
 #define DHTPORT 3
 #define DHTTYPE DHT11
@@ -27,6 +33,9 @@ bool recived = false;
 String spltCmd[3];
 
 bool exe = false;
+bool cal = false;
+byte calStep = 0;
+
 
 Adafruit_VL53L0X tof = Adafruit_VL53L0X();
 DHT_Unified dht(DHTPORT, DHTTYPE);
@@ -74,39 +83,34 @@ void Cmd_read(String* splited, bool* state, char breakReadIn, char splitIn) {
   }
 }
 
-int tofRead() {
+unsigned int tofRead() {
   tof.rangingTest(&measure, debugTof);
   if(measure.RangeStatus != 4) { return measure.RangeMilliMeter; }
   return(0);
+}
+void Cmd_clear(String* c) {
+  for(byte i = 0; i < 3; i++) { c[i] = ""; }
 }
 
 void loop() {
   Cmd_read(spltCmd, &recived, '\n', ' ');
 
-  if(recived) {
+  if(recived && !exe && !cal) {
     
-    if(spltCmd[0] == "SERVICE") {
-      if(spltCmd[1] == "-D") {
-        if(spltCmd[2] == "START") { exe = true; }
-        else if(spltCmd[2] == "STOP") { exe = false; }
-      }
+    if(spltCmd[0] == "SVC") {
+      if(spltCmd[1] == "START") { exe = true; }
     }
     
     else if(spltCmd[0] == "SYS") {
-      if(spltCmd[1] == "-C_D") { 
-        Serial.print(F("Distância da base: ")); Serial.print(spltCmd[2].toFloat()); Serial.println(F("mm"));
-      }
-      else if(spltCmd[1] == "-C_VVC") { 
-        Serial.print(F("Parâmetro de calibração: ")); Serial.print(spltCmd[2].toFloat()); Serial.println(F("mm"));
-      }
+      if(spltCmd[1] == "-D")        { Serial.print("\tDistância da base: "); Serial.print(spltCmd[2]); Serial.println(F("mm")); } //Att
+      else if(spltCmd[1] == "-VVC") { Serial.print("\tParâmetro de calibração: "); Serial.print(spltCmd[2]); Serial.println(F("mm")); calVvc = spltCmd[2].toFloat(); }
+      else if(spltCmd[1] == "-RN")  { Serial.print("\tServiço atualizado para "); Serial.print(spltCmd[2]); Serial.println(" leituras"); readNumb =  spltCmd[2].toInt(); }
+      //else if(spltCmd[1] == "-RNC")  { Serial.print("\tCalibração atualizada para "); Serial.print(spltCmd[2]); Serial.println(" leituras"); readNumbC = spltCmd[2].toInt(); }
+
     }
     
     else if(spltCmd[0] == "TOF") {
-      if(spltCmd[1] == "-C") {
-        char bf[40];
-        sprintf(bf, "Calibrar com %d medições", spltCmd[2].toInt());
-        Serial.println(bf);
-      }
+      if(spltCmd[1] == "-C") { cal = true; }
       else if(spltCmd[1] == "-R") {
         Serial.println("Reading ToF Sensor:");
         for(byte r = 0; r < spltCmd[2].toInt(); r++) {
@@ -164,13 +168,50 @@ void loop() {
         }
       }
     }
-    
     else { Serial.print("Comando desconhecido"); }
-
     recived = false;
   }
   
-  if(exe) { 
-    
+  else if(exe) {
+    if(spltCmd[0] == "D") {
+      for(byte x = 0; x < readNumb; x++) {
+        Serial.print("\t"); Serial.print(x+1); Serial.print(": "); Serial.print(tofRead()); Serial.println("mm");
+      }
+    }
+    else if(spltCmd[0] == "E") { exe = false; Serial.print("\t"); Serial.print("Service stoped"); }
   }
+
+  else if(cal) { //Error spltCmd[2] empty
+    if(calStep == 0) { Serial.println("\tCalibrando sensor ToF ("); Serial.print(spltCmd[2]); Serial.print(")..."); Serial.println("\t\tMedidas da base:\n"); calStep++; }
+    int rn = spltCmd[2].toInt();
+    if(spltCmd[0] == "D") { calStep++; }
+    if(spltCmd[0] == "E") { calStep = 0; cal = false; }
+    averageReadBase = 0;
+    if(cal == 1) {
+      for(byte r = 0; r < rn; r++) {
+        unsigned int currentRead = tofRead();
+        Serial.print("\t\t\tBase "); Serial.print(r+1); Serial.print(": "); Serial.println(currentRead); Serial.println("mm");
+        averageReadBase += currentRead;
+      }
+      averageReadBase = averageReadBase / rn;
+      calStep++;
+    }
+    if(calStep == 3) { Serial.println("\tCalibrando sensor ToF..."); Serial.println("\t\tMedidas do bloco:\n"); calStep++; }
+    else if(calStep == 5) {
+      Serial.println("\t\tMedidas do bloco:\n");
+      for(byte r = 0; r < rn; r++) {
+        unsigned int currentRead = tofRead();
+        Serial.print("\t\t\tBloco "); Serial.print(r+1); Serial.print(": "); Serial.println(currentRead); Serial.println("mm");
+        averageReadBlock += currentRead;
+      }
+      calStep = 0;
+      cal = false;
+    }
+    averageReadBlock = averageReadBlock / rn;
+    blockHeight = averageReadBase - averageReadBlock;
+    c = calVvc - blockHeight;
+  }
+
+  Cmd_clear(spltCmd);
+  recived = false;
 }
